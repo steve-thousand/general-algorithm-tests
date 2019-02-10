1dc9a944a62cb3864be3265104030a1fc7103832
Stephen Conrad <steve@Stephens-MacBook-Pro.local>
2019-02-10 13:33:52 -0600

    worst/best/avg cases and multiple array sizes

--- sorting.py
+++ sorting.py
@@ -1,12 +1,19 @@
 import random
 import time
 import sys
+from enum import Enum
+
+class Case(Enum):
+    BEST = 1
+    WORST = 2
+    AVERAGE = 3
 
 def bubble_sort(arr):
     sorted = False
     while not sorted:
         sorted = True
         for i in range(0, len(arr)):
+            # n
             if i + 1 < len(arr) and arr[i] > arr[i + 1]:
                 scratch = arr[i]
                 arr[i] = arr[i + 1]
@@ -16,8 +23,10 @@ def bubble_sort(arr):
 
 def selection_sort(arr):
     for i in range(0, len(arr)):
+        # n
         min_index = i
         for j in range(i + 1, len(arr)):
+            # n
             if arr[j] < arr[min_index]:
                 min_index = j
         scratch = arr[i]
@@ -26,14 +35,15 @@ def selection_sort(arr):
     return arr
 
 def insertion_sort(arr):
-    for i in range(0, len(arr) - 1):
-        for j in range(i + 1, 0, -1):
-            if arr[j] < arr[j - 1]:
-                scratch = arr[j]
-                arr[j] = arr[j - 1]
-                arr[j - 1] = scratch
-            else:
-                break
+    for i in range(1, len(arr)):
+        # n
+        index = i
+        while index > 0 and arr[index - 1] > arr[index]:
+            # worst case n, best case 1
+            scratch = arr[index]
+            arr[index] = arr[index - 1]
+            arr[index - 1] = scratch
+            index -= 1
     return arr
 
 TOTAL_RUNS = 10
@@ -41,33 +51,53 @@ TOTAL_RUNS = 10
 def verify(algorithm, name):
 
     # first let's test that it actually sorts correctly
-    arr = range(1, 20)
+    arr = list(range(1, 20))
     random.shuffle(arr)
     arr = algorithm(arr)
     for i in range(0, len(arr) - 1):
         if arr[i] > arr[i + 1]:
             raise Exception("NOT SORTED!")
 
-    # now let's time it
-    total = 0
-    min = sys.maxint
-    max = 0
-    for i in range(0, TOTAL_RUNS):
-        arr = range(1, 3000)
-        random.shuffle(arr)
+    print("timing " + name + " sort...")
 
-        start = time.time()
-        arr = algorithm(arr)
-        end = time.time()
+    def time_the_algorithm(algorithm, case):
+        total = 0
+        min = sys.maxsize
+        max = 0
 
-        elapsed = end - start
-        total += elapsed
-        if elapsed > max:
-            max = elapsed
-        if elapsed < min:
-            min = elapsed
+        sizes = [1000,5000,10000]
 
-    print(name + " : avg {0:.2f}s, min {1:.2f}s, max {2:.2f}s".format(total/TOTAL_RUNS, min, max))
+        for size in sizes:
+            for i in range(0, TOTAL_RUNS):
+
+                arr = list(range(1, size))
+                if case == Case.WORST:
+                    # for worst case, reverse entire array
+                    arr = list(reversed(arr))
+                elif case == Case.AVERAGE:
+                    # average case, random order
+                    random.shuffle(arr)
+
+                start = time.time()
+                arr = algorithm(arr)
+                end = time.time()
+
+                elapsed = end - start
+                total += elapsed
+                if elapsed > max:
+                    max = elapsed
+                if elapsed <= min:
+                    min = elapsed
+
+            print(name + ", n={0:} - ".format(size) + str(case) + ": avg {0:.2f}s, min {1:.2f}s, max {2:.2f}s".format(total/TOTAL_RUNS, min, max))
+
+    # worst case
+    time_the_algorithm(algorithm, Case.WORST)
+    # avg case
+    time_the_algorithm(algorithm, Case.AVERAGE)
+    # best case
+    time_the_algorithm(algorithm, Case.BEST)
+    
 
 verify(insertion_sort, "insertion")
 verify(selection_sort, "selection")
